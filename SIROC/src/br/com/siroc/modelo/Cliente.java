9a05139979f1451c6426a7e3f5e6e2e4d45a6298
lucasgarofolo <lucas.garofolo@gmail.com>
2013-09-11 21:02:25 -0300

    v1.1.4

--- SIROC/src/br/com/siroc/modelo/Cliente.java
+++ SIROC/src/br/com/siroc/modelo/Cliente.java
@@ -10,13 +10,20 @@ import javax.persistence.Table;
 @Table(name = "CLIENTE")
 public class Cliente {
 
-    
     private Long id;
-
     private String nome;
-    
+    private String inscricao_est;
+    private String celular;
+    private String telefone;
     private String endereco;
-    
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String cep;
+    private String email;
+    private String cgc;
+    private Double frete;
+
     @Id
     @GeneratedValue
     @Column(name = "cli_id")
@@ -27,8 +34,8 @@ public class Cliente {
     public void setId(Long id) {
         this.id = id;
     }
-    
-    @Column(name = "cli_nome",nullable = false)
+
+    @Column(name = "cli_nome", nullable = false, length = 150)
     public String getNome() {
         return nome;
     }
@@ -36,13 +43,103 @@ public class Cliente {
     public void setNome(String nome) {
         this.nome = nome;
     }
-    
-    @Column(name = "cli_endereco", nullable = false)
+
+    @Column(name = "cli_endereco", nullable = false, length = 80)
     public String getEndereco() {
         return endereco;
     }
 
     public void setEndereco(String endereco) {
         this.endereco = endereco;
+    }
+
+    @Column(name = "cli_inscricao_est", nullable = false, length = 20)
+    public String getInscricao_est() {
+        return inscricao_est;
+    }
+
+    public void setInscricao_est(String inscricao_est) {
+        this.inscricao_est = inscricao_est;
+    }
+
+    @Column(name = "cli_celular", nullable = true, length = 11)
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    @Column(name = "cli_telefone", nullable = true, length = 11)
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Column(name = "cli_bairro", nullable = false, length = 30)
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    @Column(name = "cli_cidade", nullable = false, length = 30)
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    @Column(name = "cli_estado", nullable = false, length = 2)
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Column(name = "cli_cep", nullable = false, length = 9)
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    @Column(name = "cli_email", nullable = true, length = 50)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Column(name = "cli_cgc", nullable = false, length = 20)
+    public String getCgc() {
+        return cgc;
+    }
+
+    public void setCgc(String cgc) {
+        this.cgc = cgc;
+    }
+
+    @Column(name = "cli_frete", nullable = true)
+    public Double getFrete() {
+        return frete;
+    }
+
+    public void setFrete(Double frete) {
+        this.frete = frete;
     }
 }
