e9d40c22c2e6933d35f2b4bc0bbbc4c49e7197ee
lucasgarofolo <lucas.garofolo@gmail.com>
2014-01-28 14:06:21 -0200

    2.1.9

--- SIROC/test/br/com/siroc/modelo/ItemTeste.java
+++ SIROC/test/br/com/siroc/modelo/ItemTeste.java
@@ -21,13 +21,36 @@ public class ItemTeste {
         new ItemBuilder().setProduto(produto);
     }
 
-    @Test(expected = NullPointerException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testaQuantidadeNula() {
-        new ItemBuilder().setQuantidade(null);
+        new ItemBuilder().setQuantidade("");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testaQuantidadeComString() {
-        new ItemBuilder().setQuantidade("");
+        new ItemBuilder().setQuantidade("asd21a2");
     }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testaQuantidadecom0(){
+        new ItemBuilder().setQuantidade("0");
+    }
+    
+    //@Test(expected = IllegalArgumentException.class)
+    @Test
+    public void testaQuantidadeCerto(){
+        new ItemBuilder().setQuantidade("1");
+    }
+    
+    
+    public void testaValorNulo(){
+        new ItemBuilder().setValor_alterado("", 2.5);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testaValorComCaracter(){
+        new ItemBuilder().setValor_alterado("1as1", 2.5);
+    }
+    
+    
 }
